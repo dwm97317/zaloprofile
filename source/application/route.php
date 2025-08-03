6491b3cce9206d0fd4a187a9fa0ef6d3b9306dda
天堂守护者 <dwm97317@163.com>
2025-08-04 00:15:33 +0800

    优化Goong地址API路由，明确指定api模块

--- source/application/route.php
+++ source/application/route.php
@@ -14,6 +14,12 @@ return [
         ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
         ':name' => ['index/hello', ['method' => 'post']],
     ],
-
+    // API路由
+    'api/goong-address/autocomplete' => 'api/GoongAddress/autocomplete',
+    'api/goong-address/place-detail' => 'api/GoongAddress/placeDetail',
+    'api/goong-address/reverse-geocode' => 'api/GoongAddress/reverseGeocode',
+    'api/goong-address/geocode' => 'api/GoongAddress/geocode',
+    'api/goong-address/provinces' => 'api/GoongAddress/getProvinces',
+    'api/goong-address/validate' => 'api/GoongAddress/validateAddress',
 ];
 
--- source/route/api.php
+++ source/route/api.php
@@ -1,15 +1,15 @@
 <?php
 use think\Route;
 
-// Goong地址API路由
-Route::group('goong-address', function () {
-    Route::get('autocomplete', 'GoongAddress/autocomplete');
-    Route::get('place-detail', 'GoongAddress/placeDetail'); 
-    Route::get('reverse-geocode', 'GoongAddress/reverseGeocode');
-    Route::get('geocode', 'GoongAddress/geocode');
-    Route::get('provinces', 'GoongAddress/getProvinces');
-    Route::post('validate', 'GoongAddress/validateAddress');
+// Goong地址API路由 - 明确指定api模块
+Route::group('api/goong-address', function () {
+    Route::get('autocomplete', 'api/GoongAddress/autocomplete');
+    Route::get('place-detail', 'api/GoongAddress/placeDetail');
+    Route::get('reverse-geocode', 'api/GoongAddress/reverseGeocode');
+    Route::get('geocode', 'api/GoongAddress/geocode');
+    Route::get('provinces', 'api/GoongAddress/getProvinces');
+    Route::post('validate', 'api/GoongAddress/validateAddress');
 });
 
-// 确保Controller类存在
-Route::rule('goong-address/:action', 'api/GoongAddress/:action', 'GET|POST'); 
+// 备用路由
+Route::rule('api/goong-address/:action', 'api/GoongAddress/:action', 'GET|POST');
